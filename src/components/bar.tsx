c53521a149294b5ed74c558dec11e4528f469ad4
Yassine-cheffai <yacincheffai@yahoo.com>
2024-02-14 23:35:14 +0100

    add button

--- src/components/bar.tsx
+++ src/components/bar.tsx
@@ -13,6 +13,7 @@ export default function Bar() {
             Ganymede
           </Typography>
           <Button color="inherit">Login</Button>
+          <Button color="inherit">Signup</Button>
         </Toolbar>
       </AppBar>
     </Box>
